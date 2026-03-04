1ea091c131ab8957794633b4d04a1c56a592e844
Kirill <kmalyautskiy@bk.ru>
2026-03-04 22:25:44 +0500

    Deploy

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'dev-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-72f1d.up.railway.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
