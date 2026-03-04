34bf0f483b249a33a2930a429f6578f0ed12dc97
Kirill <kmalyautskiy@bk.ru>
2026-03-04 23:07:53 +0500

    ARRANCARS FINAL ABOUT

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -60,3 +60,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from main import views
 
@@ -10,3 +12,7 @@ urlpatterns = [
     path('matches/', views.matches, name='matches'),
     path('about/', views.about, name='about'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
